e058aee11df2bed0a088839eb56942126d727101
fgyugt232 <js1emilebot@hotmail.com>
2024-05-06 07:30:32 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const initializeArrayWithValues = (n, value = 0) => Array(n).fill(value);
+const flattenDepth = (arr, depth = 1) =>
+  arr.reduce(
+    (a, v) =>
+      a.concat(depth > 1 && Array.isArray(v) ? flattenDepth(v, depth - 1) : v),
+    [],
+  );
